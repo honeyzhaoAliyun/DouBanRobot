dca35931b35bccf2ac4af748b41f37e93b93de43
hoenyzhaoAliyun <honey.zhao@aliyun.com>
2018-02-04 13:58:33 +0800

    init

---
+++ douban/src/main/java/com/louie/douban/util/Parameters.java
@@ -0,0 +1,11 @@
+package com.louie.douban.util;
+
+/**
+ * Total parameters
+ * Created by honliu on 2016/7/20.
+ */
+public class Parameters {
+    public static final String PATH = System.getProperties().getProperty("user.dir");
+    public static final String HOSTPAGE = "https://www.douban.com/group";
+
+}
